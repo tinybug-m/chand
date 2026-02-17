06b7b9a5a4653831d240b35b9b897fec59b701fc
tinybug-m <arshiarnjbar@gmail.com>
2026-02-17 15:48:43 +0330

    feat: add details to Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,46 @@
-import { Zap } from "lucide-react";
-import Button from "./components/atoms/Button";
+import { Zap } from 'lucide-react';
+import Button from './components/atoms/Button';
+import Typography from './components/atoms/Typography';
+import Main from './components/layout/Main';
+import Card from './components/molecules/Card';
 
 export default function Home() {
-
   return (
-    <div className="min-h-screen container m-auto">
+    <Main>
       <div className="flex w-full">
-        <div className="flex flex-1 justify-start">
-          <Button layout="icon" variant="glow">
-            <Zap className="text-cyan-400 fill-cyan-400/20" />
-          </Button>
+        <div className="flex flex-col gap-2 flex-1 justify-start">
+          <div className="flex items-center gap-2">
+            <Button layout="icon" variant="glow">
+              <Zap className="text-cyan-400 fill-cyan-400/20" />
+            </Button>
+            <Typography
+              variant="caption"
+              className="tracking-[0.4em] text-cyan-400"
+            >
+              Enterprise Market Node
+            </Typography>
+          </div>
+          <Typography variant="h1">
+            chand
+            <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-emerald-400">
+              ?
+            </span>
+          </Typography>
+          <Typography>
+            High-precision asset tracking for the Iranian market. Ultra-low
+            latency data synchronization.
+          </Typography>
         </div>
-        <div className="flex-1">2</div>
+        <div className="flex-1"/>
       </div>
-    </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </Main>
   );
 }
